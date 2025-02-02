04aaf994ffd36b8f520c27306d0c7380bec534e3
Shikha <150659967+SiyaShikha@users.noreply.github.com>
2025-02-03 00:46:12 +0530

    added part_2 function

--- 2019/day2/main.js
+++ 2019/day2/main.js
@@ -2,17 +2,34 @@ import { intCodeRunner } from "./intCodeLib.js";
 
 const input = Deno.readTextFileSync("./input.txt");
 const code = input.split(",").map((e) => +e);
-code[1] = 12;
-code[2] = 2;
 
-const main = (code) => {
+const part_1 = (code) => {
   try {
-    const { inputCode, outputCode } = intCodeRunner(code.map((n) => +n));
-    // console.log("Input Code", inputCode);
-    console.log("Output Code", outputCode);
+    code[1] = 12;
+    code[2] = 2;
+    const { outputCode } = intCodeRunner(code);
+    return outputCode;
   } catch (err) {
     console.error(err);
   }
 };
 
-main(code);
+console.log("part 1: ", part_1(code));
+
+const part_2 = (code) => {
+  let codeCopy = [...code];
+  for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+      codeCopy[1] = noun;
+      codeCopy[2] = verb;
+      const { outputCode } = intCodeRunner(codeCopy);
+      if (outputCode[0] === 19690720) {
+        return 100 * noun + verb;
+      }
+      codeCopy = code;
+    }
+    codeCopy = code;
+  }
+};
+
+console.log("part 2: ", part_2(code));
