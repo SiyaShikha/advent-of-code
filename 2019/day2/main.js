0f6a1ae849831d1324bd0709c1020410dffa2543
Shikha <150659967+SiyaShikha@users.noreply.github.com>
2025-02-02 23:40:22 +0530

    added main.js

---
+++ 2019/day2/main.js
@@ -0,0 +1,18 @@
+import { intCodeRunner } from "./intCodeLib.js";
+
+const input = Deno.readTextFileSync("./input.txt");
+const code = input.split(",").map((e) => +e);
+code[1] = 12;
+code[2] = 2;
+
+const main = (code) => {
+  try {
+    const { inputCode, outputCode } = intCodeRunner(code.map((n) => +n));
+    console.log("Input Code", inputCode);
+    console.log("Output Code", outputCode);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+main(code);
