11a7b1313da51342ef1bccfc5ffb1f8b814a8fb4
Shikha <150659967+SiyaShikha@users.noreply.github.com>
2025-02-10 00:49:50 +0530

    refactored part1

--- 2019/day4/main.js
+++ 2019/day4/main.js
@@ -1,31 +1,35 @@
 const [start, end] = [136818, 685979];
 
-const possiblePasswords = (start, end) => {
+const isValidPassword = (password) => {
+  const digits = password.toString().split("");
+  let hasAdjacentDigits = false;
+  let areDigitsIncreasing = true;
+
+  for (let i = 0; i < digits.length - 1; i++) {
+    if (digits[i] === digits[i + 1]) {
+      hasAdjacentDigits = true;
+    }
+    if (digits[i] > digits[i + 1]) {
+      areDigitsIncreasing = false;
+      break;
+    }
+  }
+
+  return hasAdjacentDigits && areDigitsIncreasing;
+};
+
+const totalValidPasswords = (start, end) => {
   let numOfPass = 0;
   for (let i = start; i <= end; i++) {
-    const digits = i.toString().split("");
-    let condition1 = false;
-    let condition2 = true;
-    for (let i = 0; i < digits.length - 1; i++) {
-      if (digits[i] === digits[i + 1]) {
-        condition1 = true;
-      }
-      if (digits[i] > digits[i + 1]) {
-        condition2 = false;
-      }
-    }
-    if (condition1 && condition2) {
-      numOfPass += 1;
+    if (isValidPassword(i)) {
+      numOfPass++;
     }
   }
   return numOfPass;
 };
 
-possiblePasswords();
-
 const part_1 = (start, end) => {
-  const numOfPass = possiblePasswords(start, end);
-  return numOfPass;
+  return totalValidPasswords(start, end);
 };
 
 console.log(part_1(start, end));
